97d8c307712d4819676499278e48c7ffa637df14
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-07-03 22:16:27 +0700

    feat: use local cache persist for query

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -1,10 +1,13 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister"
+import { QueryClient } from "@tanstack/react-query"
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      gcTime: 1000 * 60, // 1 minute
       experimental_prefetchInRender: true,
       refetchOnReconnect: false,
       refetchOnWindowFocus: false,
@@ -12,8 +15,17 @@ const queryClient = new QueryClient({
   },
 })
 
+const persister = createAsyncStoragePersister({
+  storage: window.localStorage,
+})
+
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
   )
 }
